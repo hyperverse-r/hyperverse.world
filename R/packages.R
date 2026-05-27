8adf62dc37bb7c904d5582d0893c91026edbd31d
Arthur Bréant <arthur@thinkr.fr>
2026-05-27 16:49:19 +0200

    feat(ecosystem): add Compatible packages section with supabaseR

--- R/packages.R
+++ R/packages.R
@@ -2,6 +2,10 @@ read_packages <- function() {
   yaml::read_yaml(here::here("_data/packages.yml"))
 }
 
+read_partners <- function() {
+  yaml::read_yaml(here::here("_data/partners.yml"))
+}
+
 package_card <- function(pkg, link = NULL) {
   has_link <- !is.null(link) && !identical(link, "~")
   arrow <- if (has_link) '<span class="card-arrow">↗</span>' else ""
@@ -11,6 +15,16 @@ package_card <- function(pkg, link = NULL) {
       link
     ) else '<div class="package-card">'
   wrap_close <- if (has_link) '</a>' else '</div>'
+
+  desc_html <- if (!is.null(pkg$author)) {
+    sprintf(
+      '%s<br><small style="opacity:0.7;">by @%s</small>',
+      pkg$desc,
+      pkg$author
+    )
+  } else {
+    pkg$desc
+  }
 
   sprintf(
     '%s
@@ -22,7 +36,7 @@ package_card <- function(pkg, link = NULL) {
     wrap_open,
     arrow,
     pkg$name,
-    pkg$desc,
+    desc_html,
     pkg$badge_class,
     pkg$badge,
     wrap_close
@@ -42,8 +56,17 @@ packages_grid <- function() {
   htmltools::HTML(sprintf('<div class="packages-grid">%s</div>', cards))
 }
 
+partners_grid <- function() {
+  pkgs <- read_partners()
+  cards <- paste(
+    sapply(pkgs, \(p) package_card(p, link = pkg_link(p))),
+    collapse = "\n"
+  )
+  htmltools::HTML(sprintf('<div class="packages-grid">%s</div>', cards))
+}
+
 packages_grid_home <- function() {
-  featured <- c("htmxr", "alpiner", "supar")
+  featured <- c("htmxr", "alpiner", "lucidr")
   pkgs <- read_packages()
   pkgs <- pkgs[sapply(pkgs, \(p) p$name %in% featured)]
   cards <- paste(
